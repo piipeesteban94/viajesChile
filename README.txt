0171960eba67b783d8261a8eb7a667aaec688197
piipeesteban94 <72636710+piipeesteban94@users.noreply.github.com>
2024-05-16 21:55:08 -0400

    se agrego un readme para la documentacion del proyecto

---
+++ README.txt
@@ -0,0 +1,4 @@
+##DDDSD
+
+####
+SDSDSD
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// funcion que permite que al bajar o subir la pagina el navbar se rellene de un VideoColorSpace, ya que el navbar es transparente
+// funcion que permite que al bajar o subir la pagina el navbar se rellene de un color, ya que el navbar es transparente
 $(function () {
     $(window).scroll(function () {
       var navegador = $(".navbar");
@@ -10,7 +10,7 @@ $(function () {
     });
   });
 
-// funcion que permite cambiar de color el texto de las cardds de destacados de blanco a amarillo
+// funcion que permite cambiar de color el texto de las cards de destacados de blanco a amarillo
   $(".card1")
   .mouseover(function () {
     $(".card1txt").css("color", "#f77f00");
@@ -18,3 +18,14 @@ $(function () {
   .mouseleave(function () {
     $(".card1txt").css("color", "white");
   });
+
+//funcion que arroja un alert al presionar el boton de enviar correo.
+  $('#enviarCorreo').click(function () {
+    alert("El correo fue enviado correctamente")
+})
+
+
+//funcion que muestra informacion sobre el boton de cambiar imagen del carrusel
+$(document).ready(function () {
+    $("button").tooltip()
+})
